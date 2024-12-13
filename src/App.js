36b4deb77cdab2780d8731ca30f11d3be9bfbf20
lea-bee <twigg.lt20@gmail.com>
2024-12-13 13:49:49 +0200

    Styled application

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header"></header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="sunset" />
       </main>
       <footer className="App-footer">
         <small>Coded by Leah Twigg</small>
