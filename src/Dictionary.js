cad96f45d462b2862564081eb5a5eabf09abe084
lea-bee <twigg.lt20@gmail.com>
2024-12-13 16:09:59 +0200

    Created photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photo from "./Photo.js";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiURL).then(handleResponse);
+    axios.get(apiURL).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "xa8N3JMXd749FrvhG6R36YLoJ4UAJa4PVuBIUlhWSjX4kWwEDl2hGRha";
+    let pexelsApiURL = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiURL, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -49,6 +61,7 @@ export default function Dictionary(props) {
         </section>
 
         <Results results={results} />
+        <Photo photos={photos} />
       </div>
     );
   } else {
